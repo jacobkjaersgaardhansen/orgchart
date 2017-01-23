9de9e54c42b5f05942aacba8db43efe3e21d43c2
Jacob Kjærsgaard Hansen <jacobkjaersgaardhansen@users.noreply.github.com>
2017-01-23 14:16:26 +0100

    Update include.js

--- include.js
+++ include.js
@@ -4,4 +4,6 @@ document.write('\
 <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>\
 <link rel="stylesheet" type="text/css" href="http://cdnjs.cloudflare.com/ajax/libs/select2/3.5.2/select2.min.css"></link>\
 <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/select2/3.5.2/select2.min.js"></script>\
+<script type="text/javascript" src="common_functions.js"></script>\
+<link rel="stylesheet" type="text/css" href="common_style.css"></link>\
 ');
