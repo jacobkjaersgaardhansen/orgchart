e895e9736800371f967bc8cd668c34b32f73f7a3
Jacob Kjærsgaard Hansen <jacobkjaersgaardhansen@users.noreply.github.com>
2017-01-23 13:46:21 +0100

    Create include.js

---
+++ include.js
@@ -0,0 +1,7 @@
+document.write('\
+<script src="http://d3js.org/d3.v3.min.js"></script>\
+<script src="hierarchy.js"></script>\
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>\
+<link rel="stylesheet" type="text/css" href="http://cdnjs.cloudflare.com/ajax/libs/select2/3.5.2/select2.min.css"></link>\
+<script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/select2/3.5.2/select2.min.js"></script>\
+');
